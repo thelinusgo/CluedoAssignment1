54ac6b84bb2848e80f003fa818d3b9a652cd1483
veryasian <linussgo@gmail.com>
2016-07-26 21:45:58 +1200

    Added dice class, to generate random number from 2-12.

---
+++ src/cluedo/assets/Dice.java
@@ -0,0 +1,49 @@
+
+package cluedo.assets;
+
+/**
+ * Class that represents the dice on the board.
+ * @author linus
+ *
+ */
+public class Dice {
+	
+	private int dice;
+	
+	/**
+	 * Roll a dice.
+	 */
+	public Dice(){
+		rollDice();
+	}
+	
+	/**
+	 * Roll the dice a random number between 2-12.
+	 */
+	public void rollDice(){
+	dice = 0;
+	int die1 = (int)(Math.random()*6)+1;	
+	int die2 = (int)(Math.random()*6)+1;
+	dice = die1 + die2;
+	}
+	
+	/**
+	 * Gets the die.
+	 * @return
+	 */
+	public int getDice(){
+		return this.dice;
+	}
+	
+	public static void main(String[] argc){
+		int val = 0; int avg = 0;
+		for(int i = 0; i <= 100; i++){
+			Dice d = new Dice();
+			val += d.getDice();
+			System.out.println("i: " + i + "value: "+ d.getDice());
+		}
+		avg = val/100;
+		System.out.printf("Average value: %d", avg);
+	}
+	
+}
