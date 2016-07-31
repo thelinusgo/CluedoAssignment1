993193f9da86d4023a68e3faaf668fc01c9f3568
oznaprazzi <oznaprazzi@OZNAPRAZZI>
2016-07-31 20:59:21 +1200

    fixed accusation

--- src/cluedo/arguments/Accusation.java
+++ src/cluedo/arguments/Accusation.java
@@ -14,24 +14,41 @@ public class Accusation extends Argument{
 	public Accusation(WeaponCard weapon, RoomCard room, CharacterCard character) {
 		super(weapon, room, character);
 	}
-	
+
 	/**
 	 * This checks if the current accusation was correct.
 	 * @return
 	 */
 	public boolean checkAccusation(Envelope env, Player p){
-	WeaponCard weapon = super.getWeaponCard();
-	RoomCard room = super.getRoomCard();
-	CharacterCard character = super.getCharacterCard();
-		
-	for(Card cards: env.getEnvelope()){
-		for(Card playersCard : p.getCards()){
-		
-		}
-	}
-	
-	
-	return false;
-	}
+		WeaponCard weapon = super.getWeaponCard();
+		RoomCard room = super.getRoomCard();
+		CharacterCard character = super.getCharacterCard();
 
+		boolean valid = false;
+
+		for(Card cards: env.getEnvelope()){
+			for(Card playersCard : p.getCards()){
+				if(playersCard instanceof RoomCard){
+					if(playersCard.equals(room)){
+						valid = true;
+					}else{
+						valid = false;
+					}
+				}else if(playersCard instanceof WeaponCard){
+					if(playersCard.equals(weapon)){
+						valid = true;
+					}else{
+						valid = false;
+					}
+				}else if(playersCard instanceof CharacterCard){
+					if(playersCard.equals(character)){
+						valid = true;
+					}else{
+						valid = false;
+					}
+				}
+			}
+		}
+		return valid;
+	}
 }
