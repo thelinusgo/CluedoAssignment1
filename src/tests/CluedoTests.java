5c94fb5639f67d15dc816978c7e80adba4ccdc40
veryasian <linussgo@gmail.com>
2016-08-04 16:11:21 +1200

    added junit tests

--- src/tests/CluedoTests.java
+++ src/tests/CluedoTests.java
@@ -3,6 +3,7 @@ import org.junit.*;
 
 import cluedo.assets.*;
 import cluedo.assets.Character;
+import cluedo.cards.Card;
 import cluedo.main.*;
 
 import static org.junit.Assert.*;
@@ -10,11 +11,15 @@ import java.util.*;
 
 
 public class CluedoTests {
+
+	
+	
 	/**
 	 * Tests that it can move one position.
 	 */
 	@Test
 	public void testValidMove_1() {
+		 
 		CluedoGame game = new CluedoGame();
 		try {
 			Player player = setupMockPlayer("Ralf", "Miss Scarlett", new Position(23, 13));
@@ -32,6 +37,7 @@ public class CluedoTests {
 	 */
 	@Test
 	public void testValidMove_2() {
+		 
 		CluedoGame game = new CluedoGame();
 		try {
 			Player player = setupMockPlayer("Rodolfo", "Miss Scarlett", new Position(23, 13));
@@ -49,6 +55,7 @@ public class CluedoTests {
 	 */
 	@Test
 	public void testInvalidMove_1(){
+		 
 		CluedoGame game = new CluedoGame();
 		try{
 			Player player = setupMockPlayer("Samuel", "Professor Plum", new Position(0,17));
@@ -67,6 +74,7 @@ public class CluedoTests {
 	 */
 	@Test
 	public void testInvalidMove_2(){
+		 
 		CluedoGame game = new CluedoGame();
 		try{
 			Player player = setupMockPlayer("Namuel", "Professor Plum", new Position(7,24));
@@ -86,6 +94,7 @@ public class CluedoTests {
 	 */
 	@Test
 	public void testValidMove_3(){
+		 
 		CluedoGame game = new CluedoGame();
 		int diceRoll = game.diceRoll();
 		try{
@@ -108,22 +117,110 @@ public class CluedoTests {
 	public void testGoingInRoom(){
 		CluedoGame game = new CluedoGame();
 		try{
-			Player player = setupMockPlayer("Lamuel", "Professor Plum", new Position(24,6));
-			player.setLookBack("_|");
+			Player player = setupMockPlayer("Lamuel", "Professor Plum", new Position(0,17));
+			player.setLookBack("|_");
 			game.board.getBoard()[player.position().getX()][player.position().getY()] = player.getCharacterName() + "|"; 
-		
-		
-		
-		
-		
+			game.board.move(6, -2, player);
+			System.out.println(player.position().toString());
+			assertTrue(player.isInRoom());
 		}catch(CluedoGame.InvalidMove e){
 			fail(e.getMessage());
 		}
 	}
 	
+	/**
+	 * Test that I can leave a room after entering it.
+	 */
+	@Test
+	public void testLeavingRoom(){
+		CluedoGame game = new CluedoGame();
+		try{
+			Player player = setupMockPlayer("Lamuel", "Professor Plum", new Position(0,17));
+			player.setLookBack("|_");
+			game.board.getBoard()[player.position().getX()][player.position().getY()] = player.getCharacterName() + "|"; 
+			game.board.move(6, -2, player); //moving diagonally but who cares anyway?
+			System.out.println(player.position().toString());
+			assertTrue(player.isInRoom());
+			game.board.exitRoom(player);
+			assertTrue(!player.isInRoom());
+		}catch(CluedoGame.InvalidMove e){
+			fail(e.getMessage());
+		}
+	}
 	
+	@Test
+	public void testGoingInStairs(){
+		CluedoGame game = new CluedoGame();
+		try{
+			Player player = setupMockPlayer("Lamuel", "Professor Plum", new Position(0,17));
+			player.setLookBack("|_");
+			game.board.getBoard()[player.position().getX()][player.position().getY()] = player.getCharacterName() + "|"; 
+			game.board.move(6, 2, player); //moving diagonally but who cares anyway?
+			assertTrue(player.isInRoom());
+			game.board.canMove(player);
+			//get the room that the player WILL LEAVE
+			Room oldRoom = player.getRoom();
+			game.board.moveToRoom(player);
+			assertTrue(player.isInRoom());
+			//check that the rooms Other Room equals the old room.
+			assertEquals(player.getRoom().getOtherRoom(), oldRoom);
+		}catch(CluedoGame.InvalidMove e){
+			fail(e.getMessage());
+		}	
+	}
 	
+	/**
+	 * Test when you try to go use stairs in a room that does not have them.
+	 */
+	@Test
+	public void testInvalidGoingStairs(){
+	CluedoGame game = new CluedoGame();
+	/**
+	 * TODO: NOTE: this throws a Null Pointer Exception when you try to access a room that does not have a room.
+	 */
+	try{
+		Player player = setupMockPlayer("Xamuel", "Professor Plum", new Position(0,17));
+		player.setLookBack("|_");
+		game.board.getBoard()[player.position().getX()][player.position().getY()] = player.getCharacterName() + "|"; 
+		game.board.move(6, -2, player);
+		assertTrue(player.isInRoom());
+		Room currentRoom = player.getRoom();
+		game.board.moveToRoom(player); //THIS SHOULDN'T TELEPORT YOU!
+		assertEquals(player.getRoom(), currentRoom);
+	}catch(CluedoGame.InvalidMove e){
+		fail(e.getMessage());
+	}
+  }
 	
+	/**
+	 * Test that a player must make a valid name.
+	 */
+	@Test
+	public void testInvalidCharacterName(){
+		CluedoGame game = new CluedoGame();
+		try{
+			Player player = setupMockPlayer("Namuel", "Professor Poo Poo Head", new Position(0,17));
+			System.out.println(player.getCharacterName());
+			assertTrue(player.getCharacterName() == null);
+		}catch(CluedoGame.InvalidMove e){
+			fail(e.getMessage());
+		}
+	}
+	
+	/**
+	 * Test that a player must make a valid name.
+	 */
+	@Test
+	public void testValidCharacterTa(){
+		CluedoGame game = new CluedoGame();
+		try{
+			Player player = setupMockPlayer("Namuel", "Professor Plum", new Position(0,17));
+			System.out.println(player.getCharacterName());
+			assertTrue(player.getCharacterName() == null);
+		}catch(CluedoGame.InvalidMove e){
+			fail(e.getMessage());
+		}
+	}
 	
 	/**
 	 * Setup a mock game of monopoly with a player located at a given location.
