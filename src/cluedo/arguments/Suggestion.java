33b7e9cfaa68887abd3420cceae041f7b826c6f7
oznaprazzi <oznaprazzi@OZNAPRAZZI>
2016-08-06 00:46:01 +1200

    fixed suggestion

--- src/cluedo/arguments/Suggestion.java
+++ src/cluedo/arguments/Suggestion.java
@@ -50,12 +50,12 @@ public class Suggestion extends Argument {
 					Room r2 = cc.getObject().getRoom();
 					Character c1 = cc.getObject();
 					Character c2 = r1.getCharacter();
-					c1.addRoom(r2);
+					c1.addRoom(r1);
 					if(c2 != null){
-						c2.addRoom(r1);
+						c2.addRoom(r2);
 					}
-					r1.addCharacter(c2);
-					r2.addCharacter(c1);
+					r1.addCharacter(c1);
+					r2.addCharacter(c2);
 					cc.getObject().addRoom(roomCard.getObject());
 					for(Player player : CluedoGame.getCurrentPlayers()){
 						if(!player.equals(p)){
@@ -70,12 +70,12 @@ public class Suggestion extends Argument {
 					Room r2 = wp.getObject().getRoom();
 					Weapon w1 = wp.getObject();
 					Weapon w2 = r1.getWeapon();
-					w1.addRoom(r2);
+					w1.addRoom(r1);
 					if(w2 !=  null){
-						w2.addRoom(r1);
+						w2.addRoom(r2);
 					}
-					r1.addWeapon(w2);
-					r2.addWeapon(w1);
+					r1.addWeapon(w1);
+					r2.addWeapon(w2);
 					correct = true;
 				}else if(card.equals(super.getRoomCard())){
 					correct = true;
